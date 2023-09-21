d89fb20b48cd938700279724681f8488790e8995
Salam Mubarak A <88565304+MADES99@users.noreply.github.com>
2023-09-21 03:09:31 +0100

    indented index.js

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const capital = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P
 
 const small = [
     "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"
-];
+    ];
 
 const numbers = [
     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
-];
+    ];
  
 const symbols = [
     "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"
-];
+    ];
 
 const copybtn = document.getElementById('copy-password1');
 const copybtn2 = document.getElementById('copy-password2');
